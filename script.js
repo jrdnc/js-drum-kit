dc54fdf82395f90ef1c0a80d75906aa7d2488e23
jrdnc <jrdnblvn@gmail.com>
2020-10-15 18:32:38 -0500

    attched sound and transition to key events

---
+++ script.js
@@ -0,0 +1,25 @@
+// note: backticks let us put in expression (${..}); called 'templated liberals'
+
+window.addEventListener('keydown', function(e) {
+    // console.log(e.keyCode);
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); // holds most recently pressed, valid key
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // holds html div associated with most recently pressed, valid key
+    if (!audio) return; // stop function if not one of specified keys in html
+    audio.currentTime = 0; // rewind sound to start for each 'keydown' action
+    // play audio of given key
+    audio.play();
+    key.classList.add('playing');
+    // console.log(e);
+
+});
+
+// remove transition after it's ended
+function removeTransition(e) {
+    console.log(e);
+    if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition)); //each key gets event listener added to it, then ends transition event after completed
